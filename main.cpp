40ae6bbbcbb9c65202b5ef30b85ffc953c60d1d9
Jake Houts <houtsjake@Jakes-MacBook-Pro.local>
2021-12-01 16:43:21 -0600

    2's comp

--- main.cpp
+++ main.cpp
@@ -203,3 +203,25 @@ void print()
         std::cout << assembly.at(i) << std::endl;
     }
 }
+
+int binTwosComplementToSignedDecimal(char binary[],int significantBits)
+{
+    int power = pow(2,significantBits-1);
+    int sum = 0;
+    int i;
+
+    for (i=0; i<significantBits; ++i)
+    {
+        if ( i==0 && binary[i]!='0')
+        {
+            sum = power * -1;
+        }
+        else
+        {
+            sum += (binary[i]-'0')*power;//The -0 is needed
+        }
+        power /= 2;
+    }
+
+    return sum;
+}
